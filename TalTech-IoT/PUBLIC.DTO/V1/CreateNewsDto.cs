11292687c89ac6c8c02db3bc2b996b0d1c01840a
rreint <rreint@ttu.ee>
2023-07-24 17:57:08 +0200

    image

--- TalTech-IoT/PUBLIC.DTO/V1/CreateNewsDto.cs
+++ TalTech-IoT/PUBLIC.DTO/V1/CreateNewsDto.cs
@@ -7,6 +7,7 @@ public class CreateNewsDto
     public List<ContentDto> Body { get; set; } = default!;
 
     public string Author { get; set; } = default!;
+    public string Image { get; set; } = default!;
 
     // TODO - uuri kuidas salvestada
     //public Byte[] Image { get; set; } = default!;
