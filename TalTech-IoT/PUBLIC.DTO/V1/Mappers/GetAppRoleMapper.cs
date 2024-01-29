5cab022652e95c24bee225cd88eafbff31fc9040
rreint <rreint@ttu.ee>
2024-01-29 18:08:10 +0200

    mapepr

--- TalTech-IoT/PUBLIC.DTO/V1/Mappers/GetAppRoleMapper.cs
+++ TalTech-IoT/PUBLIC.DTO/V1/Mappers/GetAppRoleMapper.cs
@@ -1,6 +1,15 @@
+using Public.DTO.Identity;
+
 namespace Public.DTO.V1.Mappers;
 
-public class GetAppRoleMapper
+public static class GetAppRoleMapper
 {
-    
+    public static Public.DTO.Identity.AppRole Map(App.Domain.Identity.AppRole role)
+    {
+        return new AppRole()
+        {
+            Id = role.Id,
+            Name = role.Name
+        };
+    }
 }
