b79d00936834ce8be94b4398792942e88308014a
rreint <rreint@ttu.ee>
2024-02-13 19:23:41 +0200

    okaodksokd

--- TalTech-IoT/App.BLL/Services/ImageStorageService/ImageStorageService.cs
+++ TalTech-IoT/App.BLL/Services/ImageStorageService/ImageStorageService.cs
@@ -33,16 +33,9 @@ public class ImageStorageService : IImageStorageService
     public async Task<List<SaveResult>> Save(SaveContent data)
     {
         // TODO: check if it even needs to send any data, if not don't bother!
-        // TODO: mby make a wrapper for the object with field done = true/false
-        var thumbnailIndex = -1;
-        Dictionary<int, string> bufferMap = new Dictionary<int, string>(); // (sequence, OriginalContent)
+        
         Dictionary<int, List<SaveImage>?> payloadDict = new Dictionary<int, List<SaveImage>?>(); // (sequence, OriginalContentImagesToSave)
         Dictionary<int, List<string>?> srcTagDict = new Dictionary<int, List<string>?>(); // siin on (sequence, OriginalContentSrcTagList [replace jaoks]) 
-
-        foreach (var originalContent in data.Items)
-        {
-            bufferMap[originalContent.Sequence] = originalContent.Content;
-        }
 
         // Get every content images which need to be saved
         foreach (var itemToSave in data.Items)
@@ -59,8 +52,6 @@ public class ImageStorageService : IImageStorageService
                 payloadDict[itemToSave.Sequence] = imagesToSave;
                 srcTagDict[itemToSave.Sequence] = srcTagList;   
             }
-            
-            
         }
 
         var CDNPayload = new CDNSaveImages()
@@ -128,12 +119,8 @@ public class ImageStorageService : IImageStorageService
                         Sequence = originalData.Sequence,
                         UpdatedContent = updatedContent
                     };    
-                
-                
                 result.Add(resultItem);
             }
-
-
         }
         return result;
     }
@@ -278,8 +265,6 @@ public class ImageStorageService : IImageStorageService
             }
         }
         
-        
-        
         // If there is no items to delete then dont do it!
         if (imagesToDelete.IsNullOrEmpty())
         {
@@ -309,235 +294,4 @@ public class ImageStorageService : IImageStorageService
             return false;
         }
     }
-
-    public string ReplaceImages(string content)
-    {
-        /*
-        // replace base string (languageString.value)
-        var srcElementRegex = "<img[^>]*>";
-        List<string> srcTagsList = new List<string> {};
-
-        MatchCollection matches = Regex.Matches(content, srcElementRegex);
-        
-        
-        foreach (Match match in matches)
-        {
-            if (match.Success)
-            {
-                srcTagsList.Add(match.Value);
-            }
-        }
-        
-        // If no images in content
-        if (srcTagsList.IsNullOrEmpty())
-        {
-            // return original content
-            return content;
-        }
-        
-        var imageServicePayload = new SavePayload()
-        {
-            Data = new List<SaveImage>()
-        };
-        
-        string base64Regex = PATTERN_BASE64IMAGE;
-
-        foreach (var tag in srcTagsList)
-        {
-            Match match = Regex.Match(tag, base64Regex);
-            if (match.Success)
-            {
-                var dto = new SaveImage()
-                {
-                    FileFormat = match.Groups[1].Value,
-                    ImageContent = match.Groups[2].Value, 
-                    Sequence = srcTagsList.IndexOf(tag) // TODO: if there are multiple same images, make some buffer list
-                };
-                imageServicePayload.data.Add(dto);
-            }
-        }
-        
-        var saveImagesResultsDto = Save(imageServicePayload);
-
-        foreach (var resultDto in saveImagesResultsDto!.Results)
-        {
-            var baseLoc = ImageStorageServiceConstants.IMAGE_PUBLIC_LOCATION;
-            var itemFromOriginalArray = srcTagsList[resultDto.Sequence];
-            var newItem = $"<img src=\"{baseLoc}{resultDto.Link}\">"; // TODO: remove this .png here
-            content = content.Replace(itemFromOriginalArray, newItem);
-        }
-        */
-        throw new NotImplementedException();
-        return content;
-    }
-
-    //private SaveImagesResultsDTO? Save(Content content)
-    //{
-        /*
-        try
-        {
-            var postPayload = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
-            var response = _httpClient.Send(new HttpRequestMessage(HttpMethod.Post, ImageStorageServiceConstants.UPLOAD_IMAGE)
-            {
-                Content = postPayload
-            });
-                
-            if (response.IsSuccessStatusCode)
-            {
-                var responseContent = response.Content.ReadAsStringAsync().Result;
-                var result = JsonConvert.DeserializeObject<SaveImagesResultsDTO>(responseContent);
-                return result;
-            }
-            else
-            {
-                // TODO: what happens if it fails?
-            }
-        }
-        catch (HttpRequestException e)
-        {
-            Console.WriteLine("\nException Caught!");
-            Console.WriteLine("Message :{0} ", e.Message);
-        }
-        return null;
-        */
-    //}
-
-    /*
-    public bool Delete(string content)
-    {
-        var payload = new List<DeleteImage>() { };
-        
-        // Regular expression pattern to match src attribute in img tags
-        string pattern = PATTERN_IMAGE_WITH_RESOURCE;
-
-        MatchCollection matches = Regex.Matches(content, pattern);
-
-        // Iterate over the matches and extract image filenames
-        foreach (Match match in matches)
-        {
-            // Extract image filename
-            string filename = match.Groups[1].Value;
-            payload.Add(new DeleteImage() {ImageName = filename});
-            Console.WriteLine($"Filename: {filename}");
-        }
-
-        // If no images were found!
-        if (payload.IsNullOrEmpty())
-        {
-            return true;
-        }
-        
-        try
-        {
-            var deletionSuccess = ExecuteDelete(payload);
-            return deletionSuccess;
-        }
-        catch (HttpRequestException e)
-        {
-            Console.WriteLine("\nException Caught when deleting images!");
-            Console.WriteLine("Message :{0} ", e.Message);
-        }
-
-        return false;
-    }
-
-    public string Update(string oldContent, string newContent)
-    {
-        // Get all old content images
-        var oldContentImages = new List<string>() { };
-        var imageInCdnPattern = PATTERN_IMAGE_WITH_RESOURCE;
-        var oldContentMatches = Regex.Matches(oldContent, imageInCdnPattern);
-        
-        // Get all new content images (img in cdn)
-
-        foreach (Match match in oldContentMatches)
-        {
-            string filename = match.Groups[1].Value;
-            oldContentImages.Add(filename);
-        }
-        
-        // Get all the new content images (img in cdn)
-
-        var newContentImages = new List<string>() { };
-        var newContentMatches = Regex.Matches(newContent, imageInCdnPattern);
-        foreach (Match match in newContentMatches)
-        {
-            var fileName = match.Groups[1].Value;
-            newContentImages.Add(fileName);
-        }
-
-        var imagesToDelete = new List<DeleteImage>() { };
-        
-        // Find items in oldContentList that are not present in newContentList
-        var itemsRemoved = oldContentImages.Except(newContentImages).ToList();
-
-        if (itemsRemoved.Any())
-        {
-            foreach (var item in itemsRemoved)
-            {
-                imagesToDelete.Add(new DeleteImage() { ImageName = item});
-            }
-        }
-        // If there are images to delete!
-        if (!imagesToDelete.IsNullOrEmpty())
-        {
-            try
-            {
-                // Delete (OLD_IMAGES)
-                var deletionSuccess = ExecuteDelete(imagesToDelete);
-                
-                // TODO: what to do here if it fails???
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine("\nException Caught when deleting images!");
-                Console.WriteLine("Message :{0} ", e.Message);
-            }
-            
-        }
-
-        // Get all the new images which are not in the content (NEW_IMAGES)
-        //var newImagesPayload = ExtractBase64Images(newContent);
-
-        // TODO: refactor this replace images shit!
-        ReplaceImages(newContent);
-
-        // Post all the new images
-
-        // return modified content
-        return newContent;
-    }
-    
-    private bool ExecuteDelete(List<DeleteImage> imagesList)
-    {
-        try
-        {
-            var postPayload = new StringContent(JsonConvert.SerializeObject(imagesList), Encoding.UTF8, "application/json");
-            var response = _httpClient.Send(new HttpRequestMessage(HttpMethod.Delete, ImageStorageServiceConstants.DELETE_IMAGE)
-            {
-                Content = postPayload
-            });
-
-            Console.WriteLine($"RESPONSE CODE: {response.StatusCode}");
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine("Images deleted successfully!");
-                var responseContent = response.Content.ReadAsStringAsync().Result;
-                bool success = bool.Parse(responseContent);
-                return success;
-            }
-            else
-            {
-                Console.WriteLine("Failed to delete images!");
-                return false;
-            }
-        }
-        catch (HttpRequestException e)
-        {
-            Console.WriteLine("\nException Caught when deleting images!");
-            Console.WriteLine("Message :{0} ", e.Message);
-            return false;
-        }
-    }
-    */
 }
