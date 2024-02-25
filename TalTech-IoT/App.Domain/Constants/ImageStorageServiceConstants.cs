10264110317e3707873f6f091fb02575534fbf25
rreint <rreint@ttu.ee>
2024-02-25 20:09:24 +0200

    update imageservcie urls

--- TalTech-IoT/App.Domain/Constants/ImageStorageServiceConstants.cs
+++ TalTech-IoT/App.Domain/Constants/ImageStorageServiceConstants.cs
@@ -11,8 +11,8 @@ public static class ImageStorageServiceConstants
      http://66.94.110.127:5000/home/dockeradmin/imagestorage/images/
      http://66.94.110.127:5000/images/upload
      */
-    public static string UPLOAD_IMAGE { get; set; } = "http://185.170.213.135/images/upload"; //"http://172.16.0.87:5000/images/upload"; // "http://localhost:5052/images/upload"; 
+    public static string UPLOAD_IMAGE { get; set; } = "http://185.170.213.135:5052/images/upload"; //"http://172.16.0.87:5000/images/upload"; // "http://localhost:5052/images/upload"; 
 
-    public static string DELETE_IMAGE { get; } = "http://172.16.0.87:5000/images/delete";
-    public static string IMAGE_PUBLIC_LOCATION { get; set; } = "http://172.16.0.87/images/";
+    public static string DELETE_IMAGE { get; } = "http://185.170.213.135:5052/images/delete";//"http://172.16.0.87:5000/images/delete";
+    public static string IMAGE_PUBLIC_LOCATION { get; set; } = "http://185.170.213.135:5052/images/"; //"http://172.16.0.87/images/";
 }
