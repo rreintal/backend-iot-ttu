6e46fe8b329dd2c3beb0476c3426d30ad71c1d0b
rreint <rreint@ttu.ee>
2024-02-27 19:01:06 +0200

    test7

--- TalTech-IoT/WebApp/Program.cs
+++ TalTech-IoT/WebApp/Program.cs
@@ -72,7 +72,7 @@ else
     connectionString = databaseUrl;
 }
 
-// a  ss sadsadasdasdasd
+// a  ss sadsadasdasdasda asiodjasoijdioasjd
 builder.Services
     .AddDbContext<AppDbContext>(options =>
     {
