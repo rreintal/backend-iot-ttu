4f0f69d93a33e09cd1e16e9b73c60c6ab849f634
rreint <rreint@ttu.ee>
2024-02-27 20:54:30 +0200

    aoao

--- TalTech-IoT/WebApp/Program.cs
+++ TalTech-IoT/WebApp/Program.cs
@@ -56,7 +56,7 @@ builder.Services.AddCors(options =>
 // TODO Identity
 //DockerDbConnection 
 //DevDbConnection
-// asadsad
+// asadsad asdokasodkas
 
 // aaa
 string? databaseUrl = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
