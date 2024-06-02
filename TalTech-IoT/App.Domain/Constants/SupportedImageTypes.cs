f7848383d2de678f96273545166c0776aa201c21
rreint <rreint@ttu.ee>
2024-06-02 16:32:33 +0300

    add ubunut native SkiaSharp

---
+++ TalTech-IoT/App.Domain/Constants/SupportedImageTypes.cs
@@ -0,0 +1,9 @@
+namespace App.Domain.Constants;
+
+public static class SupportedImageTypes
+{
+    public static List<String> SupportedImages = new List<string>()
+    {
+        "jpg", "jpeg", "png", "webp", "base64"
+    };
+}
